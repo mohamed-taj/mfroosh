d58ebe8818760188aae4913a01dfae3efd4d590a
mohamed <mohamedtaj0099@gmail.com>
2026-01-05 18:29:16 +0400

    v4

--- netlify/functions/send-enquiry.ts
+++
@@ -1,9 +0,0 @@
-import serverless from "serverless-http";
-import express from "express";
-import { handleEnquiry } from "../../server/routes/enquiry";
-
-const app = express();
-app.use(express.json());
-app.post("/", handleEnquiry);
-
-export const handler = serverless(app);
